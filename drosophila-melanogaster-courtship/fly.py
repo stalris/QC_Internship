ae05bdf596fcfd677d8ec7eb69b72c0a13983b8f
stalris <salc1990@gmail.com>
2025-06-29 02:54:15 -0400

    제 전공은 컴퓨터 공학이에요

---
+++ drosophila-melanogaster-courtship/fly.py
@@ -0,0 +1,133 @@
+import h5py as h5
+import numpy as np
+from functools import partial
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+# dummy data to test animations
+x = [0, 1, 2, 3, 4]
+y = [0, 1, 2, 3, 4]
+
+# global variables
+fig, ax = plt.subplots()
+fly = "courtship_labels.000_20190128_113421.analysis.h5"
+meece = "micepractice.000_mice.analysis.h5"
+
+def init_func():
+    print(f"hello")
+    plot_line(0)
+    ax.set_xlim(0, 5)
+    ax.set_ylim(0, 5)
+    ax.set_title(f"hello")
+
+def plot_line(frame):
+    ax.clear()
+    ax.set_xlim(0, 5)
+    ax.set_ylim(0, 5)
+    ax.set_title(f"frame: {frame}")
+    ax.plot(x[:frame+1], y[:frame+1])
+
+# find all edges and make sure there are no duplicates.
+def find_edges(filename):
+    edges = set()
+    for node1, node2 in filename['edge_names']:
+        # decode the byte strings and sort the edges.
+        # sorted expects an iterable, so create a list
+        # returns a list
+        s = sorted({node1.decode('utf-8'), node2.decode('utf-8')})
+        
+        # convert to tuple because sets cannot store lists
+        t = tuple(s)
+    
+        # add tuple to set. Should automatically fail silently if a duplicate edge is added.
+        edges.add(t)
+    return edges
+
+# plot edges for a skeleton, given a dictionary of their names as keys and coordinates as values.
+def plot_edges(node_coords, edges, axes, filename):
+    for node1, node2 in edges:
+        x = (node_coords[node1][0], node_coords[node2][0])
+        y = (node_coords[node1][1], node_coords[node2][1])
+        axes.plot(x, y)
+
+def plot_frame(frame, filename):
+    
+    ax.clear()
+    ax.set_title(f"frame: {frame}")
+
+    # find edges.
+    edges = find_edges(filename)
+
+    # node names are stored as byte strings, decode them.
+    node_names = []
+    
+    for node in filename['node_names']:
+        node = node.decode('utf-8')
+        node_names.append(node)
+
+    # store coordinates for each node, so we can later draw edges.
+    node_coords = {}
+
+    # The shape of the tracks dataset is 4-dimensional numpy array where:
+    # The first index of tracks signifies animal instances.
+    # The second index: x(0) or y(1) coordinates
+    # The third: Body parts, in order listed by filename['node_names']
+    # The fourth: The frame number of the video
+    # When looping over numpy arrays, each loop will iterate over the first index.
+    # In other words, loop over each animal instance.
+    for instance in filename['tracks'][:]:
+        
+        # For each instance, grab their x and y coordinates for every body part on this particular frame.
+        x_coordinates = instance[0, :, frame]
+        y_coordinates = instance[1, :, frame]
+    
+        # pair the coordinates together, so we can loop over them.
+        coords = zip(x_coordinates, y_coordinates)
+
+        # Loop over every pair of x and y coordinates
+        for i, (x_coord, y_coord) in enumerate(coords):
+
+            node = node_names[i]
+
+            # plot each coordinate as a point, using the 'o' argument.
+            ax.plot(x_coord, y_coord, 'o')
+
+            # label each point using data from the 'node_names' dataset.
+            ax.text(x_coord, y_coord, node)
+
+            # store coordinates for this node.
+            if node not in node_coords:
+                node_coords[node] = (x_coord, y_coord)
+            else:
+                raise Exception("duplicate node? what do? exit and start debugging!")
+        # plot edges
+        plot_edges(node_coords, edges, ax, filename)
+                    
+
+def visible_frames(filename):
+    v = []
+    # each row is an array of binary flags (0/1) on whether a given instance was detected or not.
+    for i, row in enumerate(filename['track_occupancy']):
+        if np.any(row) > 0:
+            v.append(i)
+    return v
+
+# takes an iterable (hopefully) of frames to animate.
+def animate_frames(frames, filename):
+    # FuncAnimation requires its second argument to be a function with one parameter.
+    # Since plot_frame requires 2, create a partial function to implicitly call the 2nd argument.
+    p = partial(plot_frame, filename=filename)
+
+    # FuncAnimation started an event loop by calling matplotlib.animation.TimedAnimation()
+    # The animation will run so long as a reference to it is kept alive, so return it.
+    return animation.FuncAnimation(fig, p, frames=frames, interval=1000)
+
+if __name__ == "__main__":
+    try:
+        with h5.File(fly, 'r') as f:
+            v = visible_frames(f)
+            plot_frame(v[0], f)
+            plt.show()
+            
+    except Exception as e:
+        print(f"no idea what to do with exceptions, so I'll just print them out for now: \n\te: {e}")
---
+++ origin_fishy.py
@@ -0,0 +1,172 @@
+# ihavenoideawhatimdoingdog.jpeg
+import h5py as h5
+import numpy as np
+from functools import partial
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+class SLEAP_Instance_Class:
+    def __init__(self, filename):
+        s = """
+        print this object for a bad tutorial :) e.g.:
+        fly = SLEAP_INSTANCE_CLASS('filename_here.h5')
+        print(fly)
+
+        """
+        print(s)
+
+        self.filename = filename
+    
+    # Stackoverflow and chatgpt says to use context manager protocols for handling files.
+    def __enter__(self):
+
+        # open a file and keep it open.
+        self.file = h5.File(self.filename, 'r')
+
+        # find the edges of this skeleton.
+        self.edges = self.find_edges()
+
+        # find the indices
+        self.nodes = self.find_nodes()
+
+        # Open a figure and axes, so we can plot the skeletons.
+        self.fig, self.axes = plt.subplots()
+
+        # Find all frames where there is at least one visible instance.
+        self.v = self.visible_frames()
+
+        self.axes.set_xlim(100,900)
+        self.axes.set_ylim(100,900)
+    
+        # Think this returns the object, which is required from the context manager.
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type:
+            print(f"Exception occured, of type: {exc_type}")
+            print(f"\tvalue: {exc_value}")
+            print(f"traceback: {traceback}")
+        else:
+            self.file.close()
+    
+    def __str__(self):
+        s = """
+        I designed this class to be used with the context manager protocol, so use it :)
+        For context: that's the with/as block
+        e.g. 
+        with SLEAP_INSTANCE_CLASS('filename_here.h5') as whatever_file_variable_here:
+            whatever_file_variable_here.print_datasets()
+        You can even open multiple context managers at once! e.g.:
+        with SLEAP_Instance_Class('file1.h5') as f, SLEAP_Instance_Class('file2.h5') as g:
+            f.print_datasets()
+            g.print_datasets()
+        """
+        return s 
+
+    # I'll try to include some information for datasets here.
+    def print_datasets(self):
+        print(f"File: {self.file}")
+        for dataset in self.file:
+            print(f"\tdataset: {dataset}")
+
+    def find_edges(self):
+
+        # Store edges in a set to filter out duplicates.
+        edges = set()
+
+        for node1, node2 in self.file['edge_names']:
+
+            # SLEAP stores edges as byte strings. Decode them.
+            node1 = node1.decode('utf-8')
+            node2 = node2.decode('utf-8')
+
+            # sort the edges, returns a list.
+            edge = sorted([node1, node2])
+
+            # lists can't be stored in sets, so convert to a tuple first.
+            t = tuple(edge)
+
+            # store in edges. Duplicates will fail silently.
+            edges.add(t)
+        return edges
+
+    def find_nodes(self):
+        nodes = {}
+        for i, node in enumerate(self.file['node_names']):
+            
+            # SLEAP storing things as byte strings.
+            node = node.decode('utf-8')
+            nodes[node] = i
+        return nodes
+
+    def visible_frames(self):
+        v = []
+        for i, frame in enumerate(self.file['track_occupancy']):
+            # any() checks if a row contains a single True, or equivalent, value.
+            if np.any(frame):
+                v.append(i)
+        return v
+        
+    def plot_frame(self, frame):
+
+        # Clear the figure, just incase.
+        self.axes.clear()
+        self.axes.set_xlim(100, 900)
+        self.axes.set_ylim(100, 900)
+        self.axes.set_title(f"frame: {frame}")
+
+        # Grabbing the ith visible frame.
+        # TODO, might have to rethink this logic.
+        #frame = self.v[frame]
+
+        # loop over every animal instance in this frame:
+        for instance in self.file['tracks'][:, :, :, frame]:
+             
+            # grab each set of coordinates for every body part
+            x = instance[0, :]
+            y = instance[1, :]
+
+            # plot them as points.
+            self.axes.plot(x, y, 'o')
+
+
+            # name the points
+            # for i, node in enumerate(self.file['node_names']):
+
+            #     # Sometimes the points don't have valid values.
+            #     # Could be because they are hidden from view. 
+            #     if np.isfinite(x[i]) and np.isfinite(y[i]):
+            #         self.axes.text(x[i], y[i], node.decode('utf-8'))
+            
+            # plot the edges.
+            for edge in self.file['edge_inds']:
+
+                # Just grabbing the indices of edges.
+                node1 = edge[0]
+                node2 = edge[1]
+    
+                # plot() can take 2 iterables that represent different axis'. 
+                # In this case, the first argument is an array of x coordinates
+                # The second, y coordinates. 
+                # Plots a line by matching respective elements from each iterable.
+                x = (instance[0, node1], instance[0, node2] )
+                y = (instance[1, node1], instance[1, node2] )
+                self.axes.plot(x, y)
+            
+    def animate(self, frames=None):
+
+        # If no frames are provided, animate the entire file.
+        if frames == None:
+            frames = self.v
+        elif frames > len(self.v):
+            print(f"Error: trying to animate more frames than SLEAP detected.")
+            print(f"number of frames with visible instances: {len(self.v)}")
+        return animation.FuncAnimation(self.fig, self.plot_frame, frames)
+        
+if __name__ == '__main__':
+    fishy = SLEAP_Instance_Class('drosophila-melanogaster-courtship\\fly.h5')
+    with fishy as f:
+        f.plot_frame(0)
+        animate = f.animate()
+        animate.save('fish_animation.gif', writer='pillow', fps=5)
+        plt.show()
